c49615f7948a758adafb90b7dfc3307d6750f70d
Monarch Lee <53950552+Monarch717@users.noreply.github.com>
2023-07-21 16:28:48 +0800

    added logo and some font styles

--- src/components/arbitrages.tsx
+++ src/components/arbitrages.tsx
@@ -2,43 +2,43 @@ import React, {useEffect, useState} from 'react';
 import DataTable from "react-data-table-component";
 import axios from 'axios';
 
-const url = 'http://localhost:4000/arbitrage'
+const url = 'http://45.153.242.72:4000/arbitrage'
 const paginationComponentOptions = {
     selectAllRowsItem: true,
     selectAllRowsItemText: "ALL"
-  };
-  const columns = [
+};
+const columns:any = [
     {
-      id: 1,
-      name: "Symbol",
-      selector: (row: { symbol: any, rate: any, bybit: any, okx: any; }) => row.symbol,
-      sortable: true,
-      reorder: true
+        id: 1,
+        name: "Symbol",
+        selector: (row: { symbol: any, logo:any, rate: any, bybit: any, okx: any; }) =><div className={'flex items-center gap-x-1'}><img src={row.logo} className={'w-5'} alt={'symbol logo'} /> {row.symbol}</div> ,
+        sortable: true,
+        reorder: true
     },
     {
-      id: 2,
-      name: "Rate",
-      selector: (row: { symbol: any, rate: any, bybit: any, okx: any; }) => row.rate,
-      sortable: true,
-      reorder: true
+        id: 2,
+        name: "Funding Rate",
+        selector: (row: { symbol: any, rate: any, bybit: any, okx: any; }) => <>{(Number(row.rate) < 0) ? <div className={'text-red-500 font-semibold'}>{row.rate}</div> : <div className={'text-teal-500 font-semibold'}> {row.rate} </div>}</> ,
+        sortable: true,
+        reorder: true
     },
     {
-      id: 3,
-      name: "Bybit",
-      selector: (row: { symbol: any, rate: any, bybit: any, okx: any; }) => row.bybit,
-      sortable: true,
-      right: true,
-      reorder: true
+        id: 3,
+        name: "Bybit",
+        selector: (row: { symbol: any, rate: any, bybit: any, okx: any; }) => <>{(Number(row.bybit) < 0) ? <div className={'text-red-500 font-semibold'}>{row.bybit}</div> : <div className={'text-teal-500 font-semibold'}> {row.bybit} </div>}</>,
+        sortable: true,
+        right: true,
+        reorder: true
     },
     {
-      id: 4,
-      name: "OKX",
-      selector: (row: { symbol: any, rate: any, bybit: any, okx: any; }) => row.okx,
-      sortable: true,
-      right: true,
-      reorder: true
+        id: 4,
+        name: "OKX",
+        selector: (row: { symbol: any, rate: any, bybit: any, okx: any; }) => <>{(Number(row.okx) < 0) ? <div className={'text-red-500 font-semibold'}>{row.okx}</div> : <div className={'text-teal-500 font-semibold'}> {row.okx} </div>}</>,
+        sortable: true,
+        right: true,
+        reorder: true
     }
-  ];
+];
 
 const Arbitrages = () => {
     const [data, setData] = useState([])
@@ -51,29 +51,28 @@ const Arbitrages = () => {
         try {
             const res = await axios.get(url);
             if (res?.data?.success) {
-                // console.log('data---', res.data.data.length)
                 setData(res.data.data);
             }
         } catch (e) {
             console.log(e);
         }
-        
+
 
     }
     return <div>
-        <p className='text-xl text-center mt-8'>Arbitrages</p>
+        <p className='text-xl text-center mt-8 font-bold'>Funding Rate Arbitrages</p>
         {
             data.length ?
-            <DataTable
-        title="Arbitrages"
-        columns={columns}
-        data={data}
-        // defaultSortFieldId={2}
-        // sortIcon={<SortIcon />}
-        pagination
-        paginationComponentOptions={paginationComponentOptions}
-        selectableRows
-      /> : <div>Empty</div>
+                <DataTable
+                    title=""
+                    columns={columns}
+                    data={data}
+                    // defaultSortFieldId={2}
+                    // sortIcon={<SortIcon />}
+                    pagination
+                    paginationComponentOptions={paginationComponentOptions}
+                    // selectableRows
+                /> : <div>Empty</div>
         }
     </div>
 }
--- src/components/positions.tsx
+++ src/components/positions.tsx
@@ -1,46 +1,48 @@
 import React, {useEffect, useState} from 'react';
 import DataTable from "react-data-table-component";
 import axios from 'axios';
+import bybitLogo from '../assets/images/logo/bybit-logo.png';
+import okxLogo from '../assets/images/logo/okx-logo.png';
 
-const url = 'http://localhost:4000/positions';
+const url = 'http://45.153.242.72:4000/positions';
 
 const paginationComponentOptions = {
     selectAllRowsItem: true,
     selectAllRowsItemText: "ALL"
-  };
+};
 
-  const columns = [
+const columns:any = [
     {
-      id: 1,
-      name: "Exchange",
-      selector: (row: { exchange: any, symbol: any, direction: any, size: any; }) => row.exchange,
-      sortable: true,
-      reorder: true
+        id: 1,
+        name: "Exchange",
+        selector: (row: { exchange: any, logo:any, symbol: any, direction: any, size: any; }) => <div className={'flex items-center gap-x-1 font-semibold'}><img src={row.logo} alt={'cex logo'} className={'w-5'}/> {row.exchange}</div>,
+        sortable: true,
+        reorder: true
     },
     {
-      id: 2,
-      name: "Symbol",
-      selector: (row: { exchange: any, symbol: any, direction: any, size: any; }) => row.symbol,
-      sortable: true,
-      reorder: true
+        id: 2,
+        name: "Symbol",
+        selector: (row: { exchange: any, symbol: any, direction: any, size: any; }) => row.symbol,
+        sortable: true,
+        reorder: true
     },
     {
-      id: 3,
-      name: "Direction",
-      selector: (row: { exchange: any, symbol: any, direction: any, size: any; }) => row.direction,
-      sortable: true,
-      right: true,
-      reorder: true
+        id: 3,
+        name: "Direction",
+        selector: (row: { exchange: any, symbol: any, direction: any, size: any; }) => <>{row.direction === 'short'? <div className={'text-red-500 font-semibold'}>{row.direction}</div> : <div className={'text-teal-500 font-semibold'}>{row.direction}</div> }</> ,
+        sortable: true,
+        right: true,
+        reorder: true
     },
     {
-      id: 4,
-      name: "Size",
-      selector: (row: { exchange: any, symbol: any, direction: any, size: any; }) => row.size,
-      sortable: true,
-      right: true,
-      reorder: true
+        id: 4,
+        name: "Size",
+        selector: (row: { exchange: any, symbol: any, direction: any, size: any; }) => row.size,
+        sortable: true,
+        right: true,
+        reorder: true
     }
-  ];
+];
 
 const Positions = () => {
     const [data, setData] = useState([])
@@ -53,15 +55,27 @@ const Positions = () => {
         try {
             const res = await axios.get(url);
             if (res?.data?.success) {
-                let tempData : any = [];
+                let tempData: any = [];
                 if (res.data.data?.bybit) {
                     res.data.data?.bybit.map((bybit: { symbol: any; side: any; contracts: any; }) => {
-                        return tempData.push({ exchange: 'Bybit', symbol: bybit.symbol, direction: bybit.side, size: bybit.contracts });
+                        return tempData.push({
+                            exchange: 'Bybit',
+                            logo: bybitLogo,
+                            symbol: bybit.symbol,
+                            direction: bybit.side,
+                            size: bybit.contracts
+                        });
                     })
                 }
                 if (res.data.data?.okx) {
                     res.data.data?.okx.map((okx: { symbol: any; side: any; contracts: any; }) => {
-                        return tempData.push({ exchange: 'Okx', symbol: okx.symbol, direction: okx.side, size: okx.contracts });
+                        return tempData.push({
+                            exchange: 'Okx',
+                            logo: okxLogo,
+                            symbol: okx.symbol,
+                            direction: okx.side,
+                            size: okx.contracts
+                        });
                     })
                 }
                 setData(tempData);
@@ -69,23 +83,23 @@ const Positions = () => {
         } catch (e) {
             console.log(e);
         }
-        
+
 
     }
     return <div>
-        <p className='text-xl text-center mt-8'>Positions</p>
+        <p className='text-xl text-center mt-8 font-bold'>Positions</p>
         {
             data.length ?
-            <DataTable
-        title="Positions"
-        columns={columns}
-        data={data}
-        // defaultSortFieldId={2}
-        // sortIcon={<SortIcon />}
-        pagination
-        paginationComponentOptions={paginationComponentOptions}
-        selectableRows
-      /> : <div>Empty</div>
+                <DataTable
+                    title=""
+                    columns={columns}
+                    data={data}
+                    // defaultSortFieldId={2}
+                    // sortIcon={<SortIcon />}
+                    pagination
+                    paginationComponentOptions={paginationComponentOptions}
+                    // selectableRows
+                /> : <div>Empty</div>
         }
     </div>
 }
